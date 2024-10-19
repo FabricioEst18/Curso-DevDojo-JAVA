c89c86deccb62dd3f8f7bfa00a7aa96c778eea28
Fabricio Cesar <fabricioestevam46@gmail.com>
2024-10-19 08:39:09 -0300

    Sobrescrita do método toString

---
+++ src/academy/devdojo/olafabricio/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.olafabricio.javacore.Isobrescrita.test;
+
+import academy.devdojo.olafabricio.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr.Conors");
+
+        System.out.println(anime);
+    }
+}
